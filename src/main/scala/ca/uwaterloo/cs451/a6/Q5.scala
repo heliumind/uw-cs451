2a6b8b9883cc68adf45357ea1635d49ea400e717
Henry He <henry.he@uwaterloo.ca>
2022-11-18 20:41:51 +0100

    Add Q5

---
+++ src/main/scala/ca/uwaterloo/cs451/a6/Q5.scala
@@ -0,0 +1,173 @@
+package ca.uwaterloo.cs451.a6
+
+import org.apache.log4j._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+import org.apache.spark.sql.SparkSession
+
+class Q5Conf(args: Seq[String]) extends ScallopConf(args) {
+  mainOptions = Seq(input)
+  val input = opt[String](descr = "input path", required = true)
+  val text = opt[Boolean]()
+  val parquet = opt[Boolean]()
+  verify()
+}
+
+object Q5 {
+  val log = Logger.getLogger(getClass().getName())
+
+  def main(argv: Array[String]) {
+    val args = new Q5Conf(argv)
+
+    log.info("Input: " + args.input())
+    log.info("Text: " + args.text())
+    log.info("parquet: " + args.parquet())
+
+    val conf = new SparkConf().setAppName("Q5")
+    val sc = new SparkContext(conf)
+
+    val canada = 3
+    val usa = 24
+
+    if (args.text()) {
+      val ordersRDD = sc.textFile(args.input() + "/orders.tbl")
+      val lineitemRDD = sc.textFile(args.input() + "/lineitem.tbl")
+      val nationRDD = sc.textFile(args.input() + "/nation.tbl")
+      val customerRDD = sc.textFile(args.input() + "/customer.tbl")
+
+      val nation = nationRDD
+        .map(line => {
+          val tokens = line.split("\\|")
+          (
+            tokens(0), // n_nationkey
+            tokens(1) // n_name
+          )
+        })
+        .collectAsMap()
+
+      val customer = customerRDD
+        .map(line => {
+          val tokens = line.split("\\|")
+          (
+            tokens(0), // c_custkey
+            tokens(3) // c_nationkey
+          )
+        })
+        .collectAsMap()
+
+      val nationMap = sc.broadcast(nation)
+      val customerMap = sc.broadcast(customer)
+
+      val orders = ordersRDD
+        .map(line => {
+          val tokens = line.split("\\|")
+          (
+            tokens(0), // o_orderkey
+            tokens(1) // o_custkey
+          )
+        })
+        .filter(pair => {
+          val nationkey = customerMap.value(pair._2).toInt
+          nationkey.equals(canada) || nationkey.equals(usa)
+        })
+        .map(pair => (pair._1, customerMap.value(pair._2)))
+
+      val lineitem = lineitemRDD
+        .map(line => {
+          val tokens = line.split("\\|")
+          (
+            tokens(0), // l_orderkey
+            tokens(10).substring(0, 7) // l_shipdate
+          )
+        })
+
+      lineitem.cogroup(orders)
+        .filter(pair => pair._2._2.iterator.hasNext)
+        .flatMap { case (_, (dates, nationkeys)) =>
+          dates.map(date => {
+            val nationkey = nationkeys.iterator.next()
+            val name = nationMap.value(nationkey)
+            ((nationkey.toInt, name, date), 1)
+          })
+        }
+        .reduceByKey(_ + _)
+        .sortByKey()
+        .map(pair => (pair._1._1, pair._1._2, pair._1._3, pair._2))
+        .collect()
+        .foreach(println)
+    }
+
+    if (args.parquet()) {
+      val sparkSession = SparkSession.builder.getOrCreate
+
+      val ordersDF = sparkSession.read.parquet(args.input() + "/orders")
+      val ordersRDD = ordersDF.rdd
+      val lineitemDF = sparkSession.read.parquet(args.input() + "/lineitem")
+      val lineitemRDD = lineitemDF.rdd
+      val nationDF = sparkSession.read.parquet(args.input() + "/nation")
+      val nationRDD = nationDF.rdd
+      val customerDF = sparkSession.read.parquet(args.input() + "/customer")
+      val customerRDD = customerDF.rdd
+
+      val nation = nationRDD
+        .map(row => {
+          (
+            row(0).toString, // n_nationkey
+            row(1).toString // n_name
+          )
+        })
+        .collectAsMap()
+
+      val customer = customerRDD
+        .map(row => {
+          (
+            row(0).toString, // c_custkey
+            row(3).toString // c_nationkey
+          )
+        })
+        .collectAsMap()
+
+      val nationMap = sc.broadcast(nation)
+      val customerMap = sc.broadcast(customer)
+
+      val orders = ordersRDD
+        .map(row => {
+          (
+            row(0).toString, // o_orderkey
+            row(1).toString // o_custkey
+          )
+        })
+        .filter(pair => {
+          val nationkey = customerMap.value(pair._2).toInt
+          nationkey.equals(canada) || nationkey.equals(usa)
+        })
+        .map(pair => (pair._1, customerMap.value(pair._2)))
+
+      val lineitem = lineitemRDD
+        .map(row => {
+          (
+            row(0).toString, // l_orderkey
+            row(10).toString.substring(0, 7) // l_shipdate
+          )
+        })
+
+      lineitem.cogroup(orders)
+        .filter(pair => pair._2._2.iterator.hasNext)
+        .flatMap { case (_, (dates, nationkeys)) =>
+          dates.map(date => {
+            val nationkey = nationkeys.iterator.next()
+            val name = nationMap.value(nationkey)
+            ((nationkey.toInt, name, date), 1)
+          })
+        }
+        .reduceByKey(_ + _)
+        .sortByKey()
+        .map(pair => (pair._1._1, pair._1._2, pair._1._3, pair._2))
+        .collect()
+        .foreach(println)
+    }
+
+  }
+
+}
